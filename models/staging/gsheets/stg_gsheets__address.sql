3ee48baa3138003295b6b741233e39f856b98f07
John Patrick Ombao <devfromph@gmail.com>
2024-06-12 23:39:33 +0800

    Adding som models in staging

---
+++ models/staging/gsheets/stg_gsheets__address.sql
@@ -0,0 +1,25 @@
+with
+
+table_source as (
+
+  select * from {{ source('src_gsheets', 'address') }}
+
+),
+
+final as (
+
+  select 
+
+    cast(address_id as int) as address_id,
+    address as address,
+    cast(city_id as int) as city_id,
+    cast(postal_code as int) as postal_code,
+    cast(phone as int) as address_phone_number,
+    district,
+    TO_TIMESTAMP(last_update, 'YYYY-MM-DD HH24:MI:SS') AS last_update_timestamp
+
+  from table_source
+
+)
+
+select * from final
---
+++ models/staging/gsheets/stg_gsheets__country.sql
@@ -0,0 +1,20 @@
+with 
+
+table_source as (
+
+  select * from {{ source('src_gsheets', 'country') }}
+
+),
+
+final as (
+
+  select 
+
+    cast(country_id as int) as country_id,
+    country,
+    to_timestamp(last_update, 'YYYY-MM-DD HH24:MI:SS') as last_update_timestamp
+
+  from table_source
+)
+
+select * from final 
