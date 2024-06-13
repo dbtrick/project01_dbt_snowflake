95dfee7f6d0014a7d233f714c6de509b84f44ae2
John Patrick Ombao <devfromph@gmail.com>
2024-06-13 23:24:16 +0800

    Finishing staging models

---
+++ models/staging/gsheets/stg_gsheets__payment.sql
@@ -0,0 +1,24 @@
+with 
+
+table_source as (
+
+  select * from {{ source('src_gsheets', 'payment') }}
+
+),
+
+final as (
+
+  select 
+
+    cast(payment_id as int) as payment_id,
+    cast(staff_id as int) as staff_id,
+    cast(rental_id as int) as rental_id,
+    cast(customer_id as int) as customer_id,
+    to_number(amount, 10, 2) as amount,
+    to_timestamp(payment_date, 'YYYY-MM-DD HH24:MI:SS') as payment_date
+
+  from table_source
+
+)
+
+select * from final
