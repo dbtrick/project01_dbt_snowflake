fb124d4ec4467f41e59cba9ba74d02bf39f72846
John Patrick Ombao <devfromph@gmail.com>
2024-06-19 13:52:45 +0800

    Creating payment fact table

--- models/staging/gsheets/stg_gsheets__payment.sql
+++ models/staging/gsheets/stg_gsheets__payment.sql
@@ -1,15 +1,12 @@
 with 
 
 table_source as (
-
   select * from {{ source('src_gsheets', 'payment') }}
-
 ),
 
 final as (
 
   select 
-
     cast(payment_id as int) as payment_id,
     cast(staff_id as int) as staff_id,
     cast(rental_id as int) as rental_id,
---
+++ models/warehouse/facts/fct_payment.sql
@@ -0,0 +1,39 @@
+with
+
+payment as (
+  select * from {{ ref('stg_gsheets__payment') }}
+),
+
+staff as (
+  select * from {{ ref('dim_staff') }}
+),
+
+rental as (
+  select * from {{ ref('dim_rental') }}
+),
+
+customer as (
+  select * from {{ ref('dim_customer') }}
+),
+
+final as (
+
+  select 
+    {{ dbt_utils.generate_surrogate_key(['payment.payment_id']) }} as payment_key,
+    {{ dbt_utils.generate_surrogate_key(['staff.staff_id']) }} as staff_key,
+    {{ dbt_utils.generate_surrogate_key(['rental.rental_id']) }} as rental_key,
+    {{ dbt_utils.generate_surrogate_key(['customer.customer_id']) }} as customer_key,
+
+    payment.payment_id,
+    amount,
+    payment_date
+
+  from payment
+
+  left join staff on payment.staff_id = staff.staff_id
+  left join rental on payment.rental_id = rental.rental_id
+  left join customer on payment.customer_id = customer.customer_id
+)
+
+select * from final
+
